db8b8bed6a818b0b791508cb272ca856c8a24b41
rodrigocarreoncardona <22151159@aguascalientes.tecnm.mx>
2026-06-02 10:51:16 -0600

    Cambio uso de Link en OfferBanner (#11)
    * Cambio en OfferBanner enlace
    
    * Solucion uso Link OfferBanner
    
    ---------
    
    Co-authored-by: rodrigocarreonc <rodrigocarreon21@gmail.com>

--- src/components/OfferBanner/OfferBanner.jsx
+++ src/components/OfferBanner/OfferBanner.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import './OfferBanner.css'; // Assuming you have a CSS file for styles
 
 const OfferBanner = () => (
   <div className="offer-banner">
     <div className='marquee'>
-      <h3><a href="/products">🔥 Oferta de la semana: Duro preparado $30!</a></h3>
+      <h3><Link to="/products">🔥 Oferta de la semana: Duro preparado $30!</Link></h3>
     </div>
   </div>
 );
